9680b4f42fb3419321bf3e677aca1c80a9527919
bahamute-tete <330378164@qq.com>
2022-12-31 00:36:24 +0800

    bb

--- Assets/TestResource/CommandBuffer/New Folder 3/CommandBufferTest4.cs
+++ Assets/TestResource/CommandBuffer/New Folder 3/CommandBufferTest4.cs
@@ -22,16 +22,18 @@ public class CommandBufferTest4 : MonoBehaviour
     Matrix4x4[] matrix4X4s;
     MaterialPropertyBlock pb;
 
-    Vector3[] positions;
+    Vector3[] orginalPos;
     Quaternion[] qs;
-    Vector3[] dirs; 
+    Vector3[] dirs;
+
+    bool isExpand = true;
 
     public bool isCommandBuffer = true;
 
     Vector4[] colors;
 
     
-    float d=1;
+    float d=0;
     // Start is called before the first frame update
     void Start()
     {
@@ -39,7 +41,7 @@ public class CommandBufferTest4 : MonoBehaviour
         pb = new MaterialPropertyBlock();
 
         colors = new Vector4[_Count];
-        positions = new Vector3[_Count];
+        orginalPos = new Vector3[_Count];
         qs = new Quaternion[_Count];
         dirs = new Vector3[_Count];
 
@@ -49,9 +51,9 @@ public class CommandBufferTest4 : MonoBehaviour
         for (int i = 0; i < _Count; i++)
         {
             Vector3 pos = Random.onUnitSphere * 1f;
-            positions[i] = pos;
-            tr.position = pos;
-            pb.SetColor("_Color", Random.ColorHSV());
+            orginalPos[i] = pos;
+            //tr.position = pos;
+            //pb.SetColor("_Color", Random.ColorHSV());
 
 
             Quaternion q= Quaternion.FromToRotation(Vector3.forward,pos.normalized);
@@ -70,34 +72,44 @@ public class CommandBufferTest4 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
-       
 
-        //Debug.Log("t ==" + t);
+        d += Time.deltaTime;
+
+        d = Mathf.Clamp(d, 0f, 5f);
+
+        if (d == 5f)
+        {
+            d = 5f;
+        }
+        
+
+      
+
+        //Debug.Log("d ==" + d);
 
         for (int i = 0; i < _Count; i++)
         {
-            positions[i] += dirs[i] * d;
+           Vector3 pos = orginalPos[i]+ dirs[i] * d;
 
-            matrix4X4s[i] = Matrix4x4.TRS(positions[i], qs[i], new Vector3(0.1f, 0.1f, 0.1f));
+            matrix4X4s[i] = Matrix4x4.TRS(pos, qs[i], new Vector3(0.1f, 0.1f, 0.1f));
 
             //colors[i] = new Vector4(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f), 1f);
         }
 
         if (!isCommandBuffer)
         {
-            Graphics.DrawMeshInstanced(mesh, 0, mat, matrix4X4s, _Count,pb);
+            Graphics.DrawMeshInstanced(mesh, 0, mat, matrix4X4s, _Count);
         }
         else
         {
             if (cmd != null)
             {
                 cmd.Clear();
-                Camera.main.RemoveCommandBuffer(CameraEvent.AfterForwardOpaque, cmd);
+                Camera.main.RemoveCommandBuffer(CameraEvent.AfterSkybox, cmd);
             }
 
-            cmd.DrawMeshInstanced(mesh, 0, mat, -1, matrix4X4s, _Count, pb);
-            Camera.main.AddCommandBuffer(CameraEvent.AfterForwardOpaque, cmd);
+            cmd.DrawMeshInstanced(mesh, 0, mat, 0, matrix4X4s, _Count);
+            Camera.main.AddCommandBuffer(CameraEvent.AfterSkybox, cmd);
         }
 
     }
