2ea9eb1e2bb6ef8a2daa68751828174584f83379
bahamute-tete <330378164@qq.com>
2022-12-30 22:37:05 +0800

    Update CommandBufferTest4.cs

--- Assets/TestResource/CommandBuffer/New Folder 3/CommandBufferTest4.cs
+++ Assets/TestResource/CommandBuffer/New Folder 3/CommandBufferTest4.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.UIElements;
 
 
 public class CommandBufferTest4 : MonoBehaviour
@@ -21,9 +22,16 @@ public class CommandBufferTest4 : MonoBehaviour
     Matrix4x4[] matrix4X4s;
     MaterialPropertyBlock pb;
 
+    Vector3[] positions;
+    Quaternion[] qs;
+    Vector3[] dirs; 
+
     public bool isCommandBuffer = true;
 
     Vector4[] colors;
+
+    
+    float d=1;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,19 +39,29 @@ public class CommandBufferTest4 : MonoBehaviour
         pb = new MaterialPropertyBlock();
 
         colors = new Vector4[_Count];
-
+        positions = new Vector3[_Count];
+        qs = new Quaternion[_Count];
+        dirs = new Vector3[_Count];
 
 
         matrix4X4s = new Matrix4x4[_Count];
 
         for (int i = 0; i < _Count; i++)
         {
-            Vector3 pos = Random.onUnitSphere * 5f;
+            Vector3 pos = Random.onUnitSphere * 1f;
+            positions[i] = pos;
             tr.position = pos;
             pb.SetColor("_Color", Random.ColorHSV());
-            matrix4X4s[i] = Matrix4x4.TRS(pos, Quaternion.identity, new Vector3(0.1f, 0.1f, 0.1f));
 
-            colors[i] = new Vector4(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f), 1f);
+
+            Quaternion q= Quaternion.FromToRotation(Vector3.forward,pos.normalized);
+            qs[i] = q;
+
+
+            dirs[i] = pos.normalized;
+            //matrix4X4s[i] = Matrix4x4.TRS(pos, q, new Vector3(0.1f, 0.1f, 0.1f));
+
+            //colors[i] = new Vector4(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f), 1f);
         }
 
         pb.SetVectorArray("_Color", colors);
@@ -52,6 +70,20 @@ public class CommandBufferTest4 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
+       
+
+        //Debug.Log("t ==" + t);
+
+        for (int i = 0; i < _Count; i++)
+        {
+            positions[i] += dirs[i] * d;
+
+            matrix4X4s[i] = Matrix4x4.TRS(positions[i], qs[i], new Vector3(0.1f, 0.1f, 0.1f));
+
+            //colors[i] = new Vector4(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f), 1f);
+        }
+
         if (!isCommandBuffer)
         {
             Graphics.DrawMeshInstanced(mesh, 0, mat, matrix4X4s, _Count,pb);
